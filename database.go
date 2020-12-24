97483b54fe6fa7da5ac7b6e46ca2bbd6df0d3abe
Strom Perton <stromperton@gmail.com>
2020-12-24 17:11:01 +0300

    dg

--- database.go
+++ database.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-pg/pg/v9"
 	"github.com/go-pg/pg/v9/orm"
-	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func createSchema(db *pg.DB) error {
@@ -89,14 +88,9 @@ func NewDefaultUser(id int, ref int) (User, bool) {
 
 //CreateInfic Создать историю
 func CreateInfic(author int) int {
-	nilPhoto := &tb.Photo{
-		File: tb.FromDisk("pustota.jpg"),
-	}
-
 	infic := &Infic{
 		isPublic: false,
 		AuthorID: author,
-		Image:    nilPhoto.FilePath,
 	}
 
 	_, err := db.Model(infic).Insert()
--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 		if m.Text[:2] == "/i" {
 			id, _ := strconv.Atoi(m.Text[2:])
-			message, err := SprintInfic(id)
+			message, err := SprintInfic(id, b)
 			if err != nil {
 				b.Send(m.Sender, "Инфик не существует...")
 			} else {
@@ -109,17 +109,25 @@ func main() {
 	b.Handle(&IBtnCreare, func(c *tb.Callback) {
 		b.Respond(c)
 		u := GetUser(c.Sender.ID)
-		message, _ := SprintInfic(CreateInfic(u.ID))
+		message, _ := SprintInfic(CreateInfic(u.ID), b)
 		b.Send(c.Sender, message)
 	})
 
 	b.Start()
 }
 
-func SprintInfic(id int) (tb.Sendable, error) {
+func SprintInfic(id int, b *tb.Bot) (tb.Sendable, error) {
 	inf, err := GetInfic(id)
+
+	var file tb.File
+	if inf.Image == "" {
+		file = tb.FromDisk("pustota.jpg")
+	} else {
+		file, _ = b.FileByID(inf.Image)
+	}
+
 	sendable := &tb.Photo{
-		File: tb.File{FilePath: inf.Image},
+		File: file,
 		Caption: fmt.Sprintf(`*%s*
 _%s_`, inf.Name, inf.Description),
 	}
